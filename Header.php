9b45437f4873e1e6de366260a4be6ee139b9b32e
mlucile <marie-lucile.caniard@edu.univ-fcomte.fr>
2016-04-03 15:58:19 +0200

    Version qui marche enfin

---
+++ Header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html class="no-js" lang="fr">
+	<head>
+		<title>Bibliothèque de Poudlard</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/foundation.css" />
+		<link rel="stylesheet" href="css/foundation.min.css" />
+		<link rel="stylesheet" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="Styles.css" />
+		<link rel="icon" href="img/favicon.png" />
+
+		<script src="js/vendor/modernizr.js"></script>
+	</head>
+	<body>
+		<div class="fixed" >
+			<nav class="top-bar" data-topbar role="navigation">
+				<ul class="title-area">
+					<li class="name">
+						<h1><a href="index.php">Bibliothèque</a></h1>
+					</li>
+					<li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+				</ul>	
+
+				<section class="top-bar-section">
+					<!-- Right Nav Section -->
+					<ul class="left">
+						<li class="active"><a href="recherche.php">Recherche</a></li>
+						<li class="has-dropdown active">
+							<a href="#">Gestion des livres</a>
+							<ul class="dropdown">
+								<li><a href="#">Ajouter un livre</a></li>
+								<li><a href="#">Supprimer un livre</a></li>
+							</ul>
+						</li>
+						<li class="has-dropdown active">
+							<a href="#">Gestion des emprunts</a>
+							<ul class="dropdown">
+								<li><a href="#">Ajouter un emprunt</a></li>
+								<li><a href="#">Ajouter un rendu</a></li>
+							</ul>
+						</li>
+						<li class="has-dropdown active">
+							<a href="#">Gestion des adhérents</a>
+							<ul class="dropdown">
+								<li><a href="#">Ajouter un adhérent</a></li>
+								<li><a href="#">Supprimer un adhérent</a></li>
+							</ul>
+						</li>
+					</ul>
+				</section>
+			</nav>
+		</div>
---
+++ php/config.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cladlink
+ * Date: 29/03/16
+ * Time: 09:44
+ */
+define("username","cladlink");
+define("password","Tr1f0rc31987..");
