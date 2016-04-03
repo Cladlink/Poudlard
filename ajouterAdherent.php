afe2c6ea7bddae821f805cdf6b78fcc585e9b273
mlucile <marie-lucile.caniard@edu.univ-fcomte.fr>
2016-04-03 16:30:35 +0200

    Création du squelette du site

--- ajouterAdherent.php
+++ ajouterAdherent.php
@@ -1,6 +1,18 @@
-<?php
-include "Header.php";
+<?php include "Header.php"; ?>
 
+<section>
+    <h1>Ajouter un adhérent</h1>
+    <div class="row">
+        <article class="panel large-12 medium-12 small-12 columns" >
+            <form action="formRecherche.php" method="post">
+                <div class="row">
+                    <div class="large-4 medium-4 small-4 columns"><input type="text" placeholder="Nom auteur" id="nomAdherent" name="nomAdherent"></div>
+                    <div class="large-4 medium-4 small-4 columns"><input type="text" placeholder="Titre du livre" id="adresseAdherent" name="adresseAdherent"></div>
+                </div>
+                <button class="arrondi" type="submit">Ajouter</button>
+            </form>
+        </article>
+    </div>
+</section>
 
-
-include "Footer.php";
+<?php include "Footer.php"; ?>
