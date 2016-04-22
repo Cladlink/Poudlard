2744cbaab5babefd39713a0ce26caea7eeb45f65
mlucile <marie-lucile.caniard@edu.univ-fcomte.fr>
2016-04-22 16:46:22 +0200

    arbo

---
+++ exemplaireSupprimer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mlucile
+ * Date: 22/04/16
+ * Time: 16:45
+ */
--- livreSupprimer.php
+++ livreSupprimer.php
@@ -1,18 +1,35 @@
 <?php
+session_start();
 if (isset($_GET['oeuvre']))
 {
     if (!empty($_GET['oeuvre']))
     {
         include "php/connexion.php";
 
-        $ma_commande_SQL = "SELECT ";
-
-        if($ma_connexion_mysql!= NULL)
+        $ma_commande_SQL = "SELECT OEUVRE.titreOeuvre
+                            FROM EMPRUNT
+                            JOIN EXEMPLAIRE ON EMPRUNT.idExemplaire = EXEMPLAIRE.idExemplaire
+                            JOIN OEUVRE ON EXEMPLAIRE.idOeuvre = OEUVRE.idOeuvre
+                            WHERE EMPRUNT.dateRendu IS NULL
+                            AND adherent.idAdherent = " . htmlentities($_GET['adherent']) . "
+                            ORDER BY ADHERENT.nomAdherent;
+                            ";
+        $reponse = $ma_connexion_mysql->query($ma_commande_SQL);
+        $donnees = $reponse->fetchAll();
+        if(!$donnees)
         {
-            $nbr_lignes_affectees=$ma_connexion_mysql->exec($ma_commande_SQL);
+            $ma_commande_SQL = "DELETE FROM ADHERENT
+                            WHERE idAdherent = \"" . htmlentities($_GET['adherent']) . "\";";
+            if($ma_connexion_mysql!= NULL)
+                $nbr_lignes_affectees=$ma_connexion_mysql->exec($ma_commande_SQL);
+            $_SESSION['message'] = 	"l'adherent a bien été supprimé !";
+        }
+        else
+        {
+            $_SESSION['messageError'] = "Ne peut être supprimé car des emprunts sont encore en cours ! ";
         }
 
-        $message = 	"Le livre " . htmlentities($_POST['titreLivre']) . " a bien été créé !";
-        header('location:livreGestion.php');
+
+        header('location: adherentGestion.php');
     }
 }
